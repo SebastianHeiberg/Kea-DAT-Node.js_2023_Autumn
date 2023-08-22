efbc3be00f15d350ee5f06d0ad19893f1daa4a94
SebastianHeiberg <seba3673@stud.kea.dk>
2023-08-22 10:21:24 +0200

    variabler

--- 00._miscellaneous/variables_I.js
+++ 00._miscellaneous/variables_I.js
@@ -1,8 +1,23 @@
 
 //use const whenever possible
 const scheduledBreakTime = "09:20"
+let consolelogger = 0;
 
 //Use comma in console log
 console.log("let's take a break at:", scheduledBreakTime)
+consolelogger ++
 
+//String, Number, Boolean, BigInteger, undefined, null, object, date
+//object: object, array, symbol
+
+//type coercion
+
+//the 3 wars to make strings
+
+console.log("this is the first")
+console.log('this is the secod way')
+//string template
+console.log(`this is the third way ${scheduledBreakTime}`)
+
+consolelogger += 3
 
